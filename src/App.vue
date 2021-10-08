bec492d331da3e99f5a19fb34f39a310ef666dc7
vanessawa <81361169+vanessawa@users.noreply.github.com>
2021-10-08 16:35:36 +0200

    added basket and button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
+  <h1>Welcome to our shop</h1>
+  <h2>Products</h2>
   <div v-for="Product in Products" :key="Product.id">
     {{ Product.title }}
-    <span v-text="Product.price + (Product.price * Product.tax) / 100" />
+    <span v-text="grossPrice(Product)" />
+    <button id="basket-btn" @click="onClick(Product)">add to basket</button>
+  </div>
+  <br />
+  <h2>Basket</h2>
+  <div v-for="Basketitem in Basket" :key="Basketitem.id">
+    {{ Basketitem.title }} {{ Basketitem.grossPrice }}
   </div>
 </template>
 
@@ -13,9 +21,23 @@ export default {
   data() {
     return {
       Products,
+      Basket: [],
     };
   },
   components: {},
+  methods: {
+    grossPrice(Product) {
+      let grossPrice = Math.round(
+        Product.price + (Product.price * Product.tax) / 100
+      );
+      return grossPrice;
+    },
+
+    onClick(Product) {
+      Product.grossPrice = this.grossPrice(Product);
+      this.Basket.push(Product);
+    },
+  },
 };
 </script>
 
@@ -24,8 +46,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 3rem;
+  margin-left: 2rem;
+}
+
+#basket-btn {
+  margin-left: 0.5rem;
 }
 </style>
