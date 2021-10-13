4e6890514cafc89112ed91fafdb92eaf6961a2f8
vanessawa <81361169+vanessawa@users.noreply.github.com>
2021-10-13 10:33:20 +0200

    e2e-testing

---
+++ tests/e2e/specs/frontend.js
@@ -0,0 +1,17 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe("testing site access", () => {
+  it("Visits the app root url", () => {
+    cy.visit("http://localhost:8080");
+    cy.contains("h2", "Products");
+  });
+});
+describe("clicking ALL the buttons!1", () => {
+  for (let i = 1; i <= 9; i++) {
+    it('click button [id="' + i + '"]', () => {
+      cy.visit("http://localhost:8080");
+      cy.get('button[id="' + i + '"]').click();
+      cy.get('div[id="' + i + '"]');
+    });
+  }
+});
